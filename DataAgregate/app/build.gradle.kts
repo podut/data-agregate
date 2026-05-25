8f1766f257af562f3e61fd8a203d7105c660b29e
Podut <podut@example.com>
2026-05-25 13:54:27 +0300

    revert: prod URL la http://pixcode.go.ro:8085 (HTTPS pending init-ssl.sh)

--- DataAgregate/app/build.gradle.kts
+++ DataAgregate/app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
         applicationId = "com.podut.dataagregate"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 6
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
